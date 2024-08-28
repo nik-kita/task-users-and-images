92c467788f7165af219eef56bbdff351f99aebd8
nik-kita <nik.deno.dev@gmail.com>
2024-08-28 14:52:35 +0200

    add zod, zod validation middleware

--- api/src/routers/user.router.ts
+++ api/src/routers/user.router.ts
@@ -1,15 +1,19 @@
+import { zValidator } from '@hono/zod-validator'
 import { Hono } from 'hono'
+import { z } from 'zod'
 import { UserService } from '../services/user.service'
 
-export const userRouter = new Hono().get('/', async (c) => {
-  const users = await UserService.list({
-    limit: 10,
-    offset: 0,
-    order_by: 'image_count',
-    order_direction: 'DESC'
-  })
+const UsersListOptions = z.object({
+  limit: z.number({ coerce: true }).default(10),
+  offset: z.number({ coerce: true }).default(0),
+  order_direction: z.enum(['ASC', 'DESC']).default('DESC'),
+  order_by: z.enum(['user.id', 'image_count']).default('image_count')
+})
 
-  console.log(users)
+export const userRouter = new Hono().get('/', zValidator('query', UsersListOptions), async (c) => {
+  const options = c.req.valid('query')
+  const users = await UserService.list(options)
+
   return c.json({
     users
   })
